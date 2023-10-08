ed8d455dd75623ed5908b94e12eb0834b6eb0056
Shubham Pandey <119473042+shubh895@users.noreply.github.com>
2023-10-08 16:53:06 +0530

    Add files via upload

---
+++ lecture_4II.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+int main()
+// {
+//     float a,b;
+//     cout<<"Give 2 floating point number as input"<<endl;
+//     cin>>a>>b;
+//     if(a>0.01 || b>a)
+//     {
+//         cout<<"The condition in if statement is correct";
+//     }
+// }
+{
+    // int marks;
+    // cout<<"Student , please enter the marks to calculate the grade : ";
+    // cin>>marks;
+    // if(marks>90)
+    // {
+    //     cout<<"A";
+    // }
+    // else if( marks >80)
+    // {
+    //     cout<<"B";
+    // }
+    // else if( marks >70)
+    // {
+    //     cout<<"C";
+    // }
+    // else{
+    //     cout<<"Pass";
+    // }
+    // int a,b,c,d;
+    // cout<<"Give 4 integers as input : a,b,c,d"<<endl;
+    // cin>>a>>b>>c>>d;
+    // if((a>b)&&(c>d)){
+    //     cout<<"haha";
+    // }
+    // else if(a>b && c<d)
+    // {
+    //     cout<<"hehe";
+    // }
+    // else{
+    //     cout<<"huhu";
+    // }
+    int a; 
+    cout<<"Enter a number A : "<<endl;
+    cin>>a;
+    switch(a)
+    {
+        case 1:
+        cout<<"the value of a is 1"<<endl;
+        break;
+        case 2:
+        cout<<"the value of a is 2"<<endl;
+        break;
+        case 3:
+        cout<<"the value of a is 3<<endl";
+        break;
+        default:
+        cout<<"Default will be always printed";
+        break;
+    }
+
+
+}
